e38e589d24de8855afd99f74521557c51946a18b
Sumit Singh <sumit47919@gmail.com>
2022-07-16 01:05:37 +0530

    Pushing for tomorrow

--- frontend/src/components/HomeScreen/BlogPost.jsx
+++ frontend/src/components/HomeScreen/BlogPost.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export const BlogPost = ({ item }) => {
   return (
     <div className={styles.blogpostcard}>
-      <Link to={`/${item._id}`} style={{ textDecoration: 'none' }}>
+      <Link to={`/${item._id}`} style={{ textDecoration: 'none',color:"#000000" }}>
         <div className={styles.blogpostcardInside}>
           <h3>{item.title}</h3>
         </div>
